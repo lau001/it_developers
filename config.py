df9a78a050d1d7d6eb23a525caf66ca994e7bde3
lau001 <lauragran19@gmail.com>
2015-05-13 13:49:23 +0200

    commit

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ def htmlFirst():
             <link type="text/style" rel="stylesheet" href="/style/style.css"/>
             <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
             <script type="text/javascript" src="javascript/scripts.js"></script>
-            <title>Mac Ilcapo</title>
+            <title>Mc Ilcapo</title>
         </head>
         <body class="mystyle" data-feedly-mini="yes">
-            <div id="title"><h1>Mac Ilcapo</h1></div>
+            <div id="title"><h1>Mc Ilcapo</h1></div>
             <div id="logoDiv" name="logoDiv">
 		        <img src="/images/macilcapo.png" id="logo"/>
 	        </div>
