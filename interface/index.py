3c941f5a01c81e968ac9cde804e3d710134b0e25
AndoniSanchez <asanchez156@ikasle.ehu.es>
2015-05-13 13:11:04 +0200

    7-5-15

--- interface/index.py
+++ interface/index.py
@@ -2,18 +2,20 @@ from google.appengine.api import users
 
 import config
 
-
 user = users.get_current_user()
         
 if user:
     msg = ("Welcome, %s! (<a href=\"%s\">logout</a></br><a href=\"/app\"> app </a>)" % (user.nickname(), users.create_logout_url("/")))
 else:
-    msg = ("<form action=\"/signup\">" +
-           "<input type=\"submit\" value=\"Sign Up\">" +
+    msg = ("<form action=\"/signup\">"
+           "<input type=\"submit\" value=\"Register\">"
+           "</form>"
+           "<form action=\"/login\">"
+           "<input type=\"submit\" value=\"Login\">"
             "</form>")
 
 startapp = config.htmlFirst() + \
-    ''' <div> <h1>Welcome!</h1> </div> ''' + \
+    ''' <div> <h1>Index</h1> </div> ''' + \
         msg + config.htmlEnd()
 
 
