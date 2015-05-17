1b7990977ca13c6e81ae700915bc07a040a127c6
AndoniSanchez <asanchez156@ikasle.ehu.es>
2015-05-17 16:53:53 +0200

    Continuación de AddElement y eliminado el inicio sesión de google (ahora no reconoce el usuario logeado)

--- facade/App.py
+++ facade/App.py
@@ -4,6 +4,7 @@ import config
 from dataaccess import DataAccess
 from google.appengine.api import users
 
+
 class App(webapp2.RequestHandler):
     def get(self):
         self.response.write(config.htmlFirst() + Start.menu() + Start.home() + config.htmlEnd())
@@ -17,6 +18,7 @@ class SeeElements(webapp2.RequestHandler):
         self.response.out.write('<div>' + config.htmlEnd())
 
     def getElementsOfType(self, type):
+
         user = users.get_current_user()
         elemts = DataAccess.getElements(type)
         for ele in elemts:
@@ -25,10 +27,10 @@ class SeeElements(webapp2.RequestHandler):
             self.response.out.write('<tr><td><img src="' + ele.photo + '"/></td>')
             if user == "admin":
                 self.response.out.write("<td><input type='button' class='button' value='Delete' onclick=\"deleteElement(\'" + ele.idElement + "\')\"/>")
-            #elif user:
-            self.response.out.write("<td><input type='button' class='button' value='-' onclick=\"minusElement(\'" + ele.idElement + "\')\"/>")
-            self.response.out.write('<input type="text" id="element" value=0')
-            self.response.out.write("<td><input type='button' class='button' value='+' onclick=\"plusElement(\'" + ele.idElement + "\')\"/>")
+            elif user:
+                self.response.out.write("<td><input type='button' class='button' value='-' onclick=\"minusElement(\'" + ele.idElement + "\')\"/>")
+                self.response.out.write('<input type="text" id="element" value=0')
+                self.response.out.write("<td><input type='button' class='button' value='+' onclick=\"plusElement(\'" + ele.idElement + "\')\"/>")
             self.response.out.write('<td></tr></table>')
 
 app = webapp2.WSGIApplication([
